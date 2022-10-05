9fd223a7202353a73ea2efcf6cf8162ee43669e7
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-05 09:42:18 +0300

    First Code in Php class

---
+++ first_program/first_code.php
@@ -0,0 +1,9 @@
+<?php
+// Displaying my details
+$username = "Denzel Gitonga";
+$year = 3;
+// display these details
+echo "My name is $username";
+echo "<br>";
+echo "I am a year $year student";
+?>
