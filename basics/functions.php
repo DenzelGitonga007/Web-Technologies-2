39ab097b2470dc7576dd59fb9a7ae769a0710729
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-19 12:18:06 +0300

    functions

---
+++ basics/functions.php
@@ -0,0 +1,10 @@
+<?php
+// A function to calculate sum of two numbers
+function sum($x, $y) {
+    $name = "Denzel Emmanuel";
+    $val = $x + $y;
+    echo $name . " " . $val;
+}
+// call the function
+sum(30, 40);
+?>
