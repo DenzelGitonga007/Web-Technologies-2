51c23aedd4afab8bc8ac6d68e613904ef5b3feb5
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-19 10:41:17 +0300

    2d array

---
+++ arrays/2d.php
@@ -0,0 +1,12 @@
+<!-- Displaying elements in a 2d array -->
+<?php
+// Initialize the array
+$marks = array(
+    array(50, 80),
+    array(75, 42),
+    array(30, 92),
+    array(54, 73)
+);
+// Access an element
+echo $marks[2][1];
+?>
