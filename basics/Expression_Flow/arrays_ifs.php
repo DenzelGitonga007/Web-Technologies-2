306166ae3a3b6959c13ab15b309dc49db9b9ae25
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-26 14:30:57 +0300

    improved exam score

--- basics/Expression_Flow/arrays_ifs.php
+++ basics/Expression_Flow/arrays_ifs.php
@@ -12,8 +12,7 @@ $mean_3 = ($marks[2][3] + $marks[2][4] + $marks[2][5])/3;
 // For first stuent
 if ($mean_1 >= 70 and $mean_1 <= 100) {
     $grade_1 = "excellent";
-    echo $marks[0][2]. " has a mean score of  " . $mean_1 . " which is ";
-    echo "<br>";
+    
 }
 elseif ($mean_1 >= 60 and $mean_1 <= 69) {
     $grade_1 = "very good";
@@ -22,65 +21,60 @@ elseif ($mean_1 >= 60 and $mean_1 <= 69) {
 }
 elseif ($mean_1 >= 50 && $mean_1 <= 59) {
     $grade_1 = "good";
-    echo $marks[0][2]. " has a mean score of  " . $mean_1 . " which is a " . $grade_1;
-    echo "<br>";
+    
 }
 elseif ($mean_1 >= 40 && $mean_1 <= 49) {
     $grade_1 = "nice attempt";
-    echo $marks[0][2]. " has a mean score of  " . $mean_1 . " which is a " . $grade_1;
-    echo "<br>";
+    
 }
 else {
     $grade_1 = "repeat";
-    echo $marks[0][2]. " has a mean score of  " . $mean_1 . " which is a " . $grade_1;
-    echo "<br>";
+    
 }
-// For secon stuent
+// For second stuent
 if ($mean_2 >= 70 and $mean_2 <= 100) {
     $grade_2 = "excellent";
-    echo $marks[1][2]. " has a mean score of  " . $mean_2 . " which is " . $grade_2;
-    echo "<br>";
+    
 }
 elseif ($mean_2 >= 60 and $mean_2 <= 69) {
     $grade_2 = "very good";
-    echo $marks[1][2]. " has a mean score of  " . $mean_2 . " which is " . $grade_2;
-    echo "<br>";
+    
 }
 elseif ($mean_2 >= 50 && $mean_2 <= 59) {
     $grade_2 = "good";
-    echo $marks[1][2]. " has a mean score of  " . $mean_2 . " which is a " . $grade_2;
-    echo "<br>";
+    
 }
 elseif ($mean_2 >= 40 && $mean_2 <= 49) {
     $grade_2 = "nice attempt";
-    echo $marks[1][2]. " has a mean score of  " . $mean_2 . " which is a " . $grade_2;
-    echo "<br>";
+    
 }
 else {
     $grade_2 = "repeat";
-    echo $marks[1][2]. " has a mean score of  " . $mean_2 . " which is a " . $grade_2;
-    echo "<br>";
+    
 }
 // For mean three
 if ($mean_3 >= 70 and $mean_3 <= 100) {
     $grade_3 = "excellent";
-    echo $marks[2][2]. " has a mean score of  " . $mean_3 . " which is ". $grade_3;
+    
 }
 elseif ($mean_3 >= 60 and $mean_3 <= 69) {
     $grade_3 = "very good";
-    echo $marks[2][2]. " has a mean score of  " . $mean_3 . " which is " . $grade_3;
+    
 }
 elseif ($mean_3 >= 50 && $mean_3 <= 59) {
     $grade_3 = "good";
-    echo $marks[2][2]. " has a mean score of  " . $mean_3 . " which is a " . $grade_3;
+    
 }
 elseif ($mean_3 >= 40 && $mean_3 <= 49) {
     $grade_3 = "nice attempt";
-    echo $marks[2][2]. " has a mean score of  " . $mean_3 . " which is a " . $grade_3;
+    
 }
 else {
     $grade_3 = "repeat";
-    echo $marks[2][2]. " has a mean score of  " . $mean_3 . " which is a " . $grade_3;
 }
-
+echo $marks[0][2] . " had a mean score of ". $mean_1. " and the comment is " . $grade_1;
+echo "<br>";
+echo $marks[1][2] . " had a mean score of ". $mean_2. " and the comment is " . $grade_2;
+echo "<br>";
+echo $marks[2][2] . " had a mean score of ". $mean_3. " and the comment is " . $grade_3;
 ?>
